41e9303c2ad34d158406a1f19e2b5047f3a9a022
horaceo1b <horaceken0@gmail.com>
2025-07-10 19:00:35 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { image } from "../data/user"; // 👈 Import image
 
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <h2>About</h2>
+      <img src={image} alt="User" />
     </div>
   );
 }
+
+export default About;
+// Use the imported image
